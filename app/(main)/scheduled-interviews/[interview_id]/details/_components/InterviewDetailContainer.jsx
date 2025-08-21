bed2631fda86ab10ac7bf3b13ce391722cdad982
Reddy <reddy@Reddys-MacBook-Air.local>
2025-08-21 23:59:37 +0530

    12 commit

---
+++ app/(main)/scheduled-interviews/[interview_id]/details/_components/InterviewDetailContainer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Calendar, Clock, MessageCircleQuestion } from 'lucide-react'
+import moment from 'moment'
+
+
+
+function InterviewDetailContainer({interviewDetail}) {
+  return (
+    <div className='p-5  rounded-lg bg-white mt-5'>
+      <h2 className='text-lg font-bold'>{interviewDetail?.jobPosition}</h2>
+      <div className='mt-4 flex items-center justify-between lg:pr-52'>
+        <div >
+            <h2 className='text-gray-500 text-xs'>Duration</h2>
+            <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />{interviewDetail?.duration} min</h2>
+        </div>
+        <div >
+            <h2 className='text-gray-500 text-xs'>Created On</h2>
+            <h2 className='flex text-sm font-bold items-center gap-2'><Calendar className='h-4 w-4' />{moment(interviewDetail?.created_at).format('MMM DD ,YYYY')} </h2>
+        </div>
+        {interviewDetail?.type && <div >
+            <h2 className='text-gray-500 text-xs'>Type</h2>
+            <h2 className='flex text-sm font-bold items-center gap-2'><Clock className='h-4 w-4' />{interviewDetail?.type} </h2>
+        </div>}
+        
+      </div>
+      <div className='mt-5'>
+        <h2 className='font-bold'>Job Description</h2>
+        <p className='text-sm leading-6'>{interviewDetail?.jobDescription}</p>
+      </div>
+      <div className='mt-5'>
+            <h2 className='font-bold'>Interview Questions</h2>
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-3 mt-3'>
+            {interviewDetail?.questionList.map((item,index)=>(
+                <h2 className='text-sm'>{index+1}.{item.question}</h2>
+            ))} 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default InterviewDetailContainer
--- app/(main)/scheduled-interviews/[interview_id]/details/page.jsx
+++ app/(main)/scheduled-interviews/[interview_id]/details/page.jsx
@@ -1,32 +1,37 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import { supabase } from '@/Services/SupabaseClient'; // Adjust the import based on your project structure
 import { useUser } from '@/app/Provider'; // Assuming you have a custom hook for user context
+import InterviewDetailContainer from './_components/InterviewDetailContainer';
 
-function interviewDetail() {
+
+function InterviewDetail() {
     const { interview_id } = useParams(); // Assuming you're using react-router for routing
     const { user } = useUser();
+    const [interviewDetail, setInterviewDetail] = useState();
 
     useEffect(() => {
-        user && GetInterviewDetails();
+        user && GetInterviewDetail();
     }, [user]);
     
-    const GetInterviewDetails = async () => {
+    const GetInterviewDetail = async () => {
         const result = await supabase
         .from('all_interviews')
-        .select('jobPosition,duration,interview_id,interview-feedback(userEmail)')
+        .select(`jobPosition,jobDescription,type,questionList,duration,interview_id,created_at,interview-feedback(userEmail,userName,feedback,created_at)`)
         .eq('email', user?.email)
         .eq('interview_id', interview_id)
-        .order('id', { ascending: false });
+        // .order('id', { ascending: false });
 
+        setInterviewDetail(result?.data[0]);
         console.log(result);
     }
   return (
-    <div>
-      interview detail
+    <div className='mt-5'>
+      <h2 className='text-2xl font-bold'>Interview Details</h2>
+      <InterviewDetailContainer interviewDetail={interviewDetail} />
     </div>
   )
 }
 
-export default interviewDetail
+export default InterviewDetail
